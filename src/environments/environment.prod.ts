c0e9419183f75989e252034dd59155e45f0254ed
Paris Ward <wardparis.pw@gmail.com>
2018-07-28 16:23:58 -0700

    added firebase/firestore and data fetching methods

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase : {
+    apiKey: "AIzaSyBNekiRh-SRKZWXQKaRvgn_SmJOpbKrcUk",
+    authDomain: "client-panel-prod-9dc99.firebaseapp.com",
+    databaseURL: "https://client-panel-prod-9dc99.firebaseio.com",
+    projectId: "client-panel-prod-9dc99",
+    storageBucket: "client-panel-prod-9dc99.appspot.com",
+    messagingSenderId: "894363923419"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase : {
+    apiKey: "AIzaSyBNekiRh-SRKZWXQKaRvgn_SmJOpbKrcUk",
+    authDomain: "client-panel-prod-9dc99.firebaseapp.com",
+    databaseURL: "https://client-panel-prod-9dc99.firebaseio.com",
+    projectId: "client-panel-prod-9dc99",
+    storageBucket: "client-panel-prod-9dc99.appspot.com",
+    messagingSenderId: "894363923419"
+  }
 };
 
 /*
