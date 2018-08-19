7ce41413f57d52d1fbc0df8fda7a36baa9f4c0e7
Paris Ward <wardparis.pw@gmail.com>
2018-08-18 23:34:30 -0700

    added method for adding client to firestore from interface, began client details

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { FlashMessagesModule } from 'angular2-flash-messages';
 
 import { environment } from '../environments/environment';
 import { AngularFireModule } from 'angularfire2';
@@ -40,6 +41,7 @@ import { AppRoutingModule } from './/app-routing.module';
     BrowserModule,
     FormsModule,
     AppRoutingModule,
+    FlashMessagesModule.forRoot(),
     AngularFireModule.initializeApp(environment.firebase,'clientpanel'),
     AngularFirestoreModule,
     AngularFireAuthModule
