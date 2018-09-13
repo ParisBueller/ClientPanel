f0ff0cca6ef4597dd78482440b1405d559a36efa
Paris Ward <wardparis.pw@gmail.com>
2018-09-13 08:54:26 -0700

    added settings page and local storage for settings

--- src/app/services/settings.service.ts
+++ src/app/services/settings.service.ts
@@ -8,12 +8,24 @@ import { Settings } from '../models/Settings';
 export class SettingsService {
   settings: Settings = {
     allowRegistration: true,
-    disableBalanceOnAdd: false,
-    disableBalanceOnEdit: false
+    disableBalanceOnAdd: true,
+    disableBalanceOnEdit: true
   }
-  constructor() { }
+  constructor() { 
+    //check in local storage for settings
+    if(localStorage.getItem('settings') != null) {
+      //if local storage has settings, assign those settings
+      this.settings = JSON.parse(localStorage.getItem('settings'));
+    }
+  }
 
   getSettings(): Settings {
     return this.settings;
+  }
+
+  changeSettings(settings: Settings) {
+    //local storage can only hold strings, when we store it we stringify it with JSON.stringify,
+    //when we retrieve it we turn it back into a JSON object with JSON.parse
+    localStorage.setItem('settings', JSON.stringify(settings));
   }
 }
